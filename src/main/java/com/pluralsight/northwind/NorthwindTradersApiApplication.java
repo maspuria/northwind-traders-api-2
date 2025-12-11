2f2afbbd8c6178ec3b75e1a75ee722b4edae772a
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-11 13:37:07 -0600

    update home controller location and pom.xml commit

--- src/main/java/com/pluralsight/northwind/NorthwindTradersApiApplication.java
+++ src/main/java/com/pluralsight/northwind/NorthwindTradersApiApplication.java
@@ -2,12 +2,27 @@ package com.pluralsight.northwind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class NorthwindTradersApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NorthwindTradersApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(NorthwindTradersApiApplication.class, args);
 
+        String[] beanNames = context.getBeanDefinitionNames();
+
+        System.out.println("\nMy Application Beans:");
+        for (String beanName : beanNames) {
+            Object bean = context.getBean(beanName);
+            String packageName = bean.getClass().getPackageName();
+
+
+            if (packageName.startsWith("com.pluralsight.northwind")) {
+                System.out.println(beanName + " -> " + bean.getClass().getSimpleName());
+            }
+        }
+    }
 }
+
+
--- src/main/java/com/pluralsight/northwind/controllers/HomeController.java
+++ src/main/java/com/pluralsight/northwind/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.pluralsight.northwind;
+package com.pluralsight.northwind.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
